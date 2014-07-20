6684dcde0c02414137eeb1ed3fead08bcfaed401
Matt Bargar <mbargar@gmail.com>
2014-07-20 13:35:12 -0400

    GET for specific actor working

--- server.js
+++ server.js
@@ -104,6 +104,61 @@ router.route('/actors')
         });
     });
 
+// on routes that end in /actors/:actor_id
+// ----------------------------------------------------
+router.route('/actors/:actor_id')
+
+    // get the actor with that id (accessed at GET http://localhost:8080/api/actors/:actor_id)
+    .get(function(req, res) {
+
+        Actor
+            .find({ where: { actor_id: req.params.actor_id } })
+            .complete(function(err, actor) {
+                if (!!err) {
+                    console.log('An error occurred while searching for actor:', err)
+                } else if (!actor) {
+                    console.log('No user with the username "john-doe" has been found.')
+                    res.json({ message: 'Actor not found!' });
+                } else {
+                    res.json(actor.values);
+                }
+        });
+    });
+
+    // update the bear with this id (accessed at PUT http://localhost:8080/api/bears/:bear_id)
+//    .put(function(req, res) {
+//
+//        // use our bear model to find the bear we want
+//        Bear.findById(req.params.bear_id, function(err, bear) {
+//
+//            if (err)
+//                res.send(err);
+//
+//            bear.name = req.body.name; 	// update the bears info
+//
+//            // save the bear
+//            bear.save(function(err) {
+//                if (err)
+//                    res.send(err);
+//
+//                res.json({ message: 'Bear updated!' });
+//            });
+//
+//        });
+//    })
+//
+//    // delete the bear with this id (accessed at DELETE http://localhost:8080/api/bears/:bear_id)
+//    .delete(function(req, res) {
+//        Bear.remove({
+//            _id: req.params.bear_id
+//        }, function(err, bear) {
+//            if (err)
+//                res.send(err);
+//
+//            res.json({ message: 'Successfully deleted' });
+//        });
+//    });
+
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
 app.use('/api', router);
